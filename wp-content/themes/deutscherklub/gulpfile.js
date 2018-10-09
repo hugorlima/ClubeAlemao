9b96d270246bbb59ea964fda15462fc301431b65
hugorlima <hugor.souzalima@gmail.com>
2018-10-08 23:37:41 -0300

    internal pages

--- wp-content/themes/deutscherklub/gulpfile.js
+++ wp-content/themes/deutscherklub/gulpfile.js
@@ -30,7 +30,7 @@
 // START Editing Project Variables.
 // Project related.
 var project                 = 'Clube Alemão'; // Project Name.
-var projectURL              = 'localhost/ClubeAlemao'; // Local project URL of your already running WordPress site. Could be something like local.dev or localhost:8888.
+var projectURL              = 'http://localhost/ClubeAlemao'; // Local project URL of your already running WordPress site. Could be something like local.dev or localhost:8888.
 var productURL              = './'; // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 
 // Translation related.
