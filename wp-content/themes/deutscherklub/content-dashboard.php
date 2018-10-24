dfbf786334986e56484d28b26138c6c7b8b606d1
hugorlima <hugor.souzalima@gmail.com>
2018-10-23 23:04:19 -0300

    Home + Internal + dashboard

---
+++ wp-content/themes/deutscherklub/content-dashboard.php
@@ -0,0 +1,372 @@
+<section class="internal_header dashboard">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="user_data">
+					<p class="page_title">Painel do Usuário</p>
+					<p class="name">Thiago Marinho Falcão de Souza</p>
+					<p class="role">Sócio <span>Nº 321</span></p>
+					<img src="<?php bloginfo('stylesheet_directory') ?>/assets/img/thiago.png" alt="">
+					<div class="wrap-section-title_internal type-c">
+						<h2>Olá, Thiago.</h2>
+						<h3>Do que você precisa?</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="container page_internal">
+	<div class="row">
+		<div class="card-columns">
+			<div class="accordion" id="accordionFunctions">
+				<div class="card">
+					<div class="card-header" id="headingSix">
+						<h5 class="mb-0">
+							<a data-target="#collapseSix" href="javascript:void(0);">
+								<i class="far fa-calendar-check"></i>Eventos
+							</a>
+						</h5>
+					</div>
+					<div id="collapseSix">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="next_events">
+									<h3>Próximos eventos</h3>
+									<ul class="list">
+										<li>
+											<a href="#">
+												<p class="event_date"><span class="date"><i class="far fa-calendar-alt"></i> 22/05 - sex</span> <span class="hour"><i class="far fa-clock"></i> 22h</span></p>
+												<p class="title">Projeto Clube de Salão com <strong>Maestro Spok e Gafieira de Salão</strong></p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="event_date"><span class="date"><i class="far fa-calendar-alt"></i> 22/05 - sex</span> <span class="hour"><i class="far fa-clock"></i> 22h</span></p>
+												<p class="title">Projeto Clube de Salão com <strong>Maestro Spok e Gafieira de Salão</strong></p>
+											</a>
+										</li>
+									</ul>
+								</div>
+								<div class="new_inserts">
+									<h3>Últimas inserções</h3>
+									<ul class="list">
+										<li>
+											<a href="#">
+												<p class="event_date"><span class="date"><i class="far fa-calendar-alt"></i> 22/05 - sex</span> <span class="hour"><i class="far fa-clock"></i> 22h</span></p>
+												<p class="title">Projeto Clube de Salão com <strong>Maestro Spok e Gafieira de Salão</strong></p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="event_date"><span class="date"><i class="far fa-calendar-alt"></i> 22/05 - sex</span> <span class="hour"><i class="far fa-clock"></i> 22h</span></p>
+												<p class="title">Projeto Clube de Salão com <strong>Maestro Spok e Gafieira de Salão</strong></p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingOne">
+						<h5 class="mb-0">
+							<a data-target="#collapseOne" href="javascript:void(0);">
+								<i class="fas fa-users"></i>Convidados
+							</a>
+						</h5>
+					</div>
+					<div id="collapseOne">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="next_events">
+									<h3>Hoje <span class="total">Total: 63</span></h3>
+									<ul class="list_guests">
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+								<div class="new_inserts">
+									<h3>Amanhã <span class="total">Total: 25</span></h3>
+									<ul class="list_guests">
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingTwo">
+						<h5 class="mb-0">
+							<a data-target="#collapseTwo" href="javascript:void(0);">
+								<i class="far fa-newspaper"></i>Notícias
+							</a>
+						</h5>
+					</div>
+					<div id="collapseTwo">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<ul class="list_news">
+								<li>
+									<a href="#">
+										<p class="date">22/06/2018</p>
+										<p class="title">transmissão dos jogos da copa do mundo 2018</p>
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<p class="date">22/06/2018</p>
+										<p class="title">reforma da quadra de tênis é concluída e aprovada por todos.</p>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingFive">
+						<h5 class="mb-0">
+							<a data-target="#collapseFive" href="javascript:void(0);">
+								<i class="fas fa-search"></i>Transparência
+							</a>
+						</h5>
+					</div>
+					<div id="collapseFive">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="new_inserts">
+									<h3>Últimas inserções</h3>
+									<ul class="list_news">
+										<li>
+											<a href="#">
+												<p class="date">22/06/2018</p>
+												<p class="title">NONOONONO nONONONON NO NONONONO NONO</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="date">22/06/2018</p>
+												<p class="title">NONON NON NO ONONONON NONONNON NONOON ON OON NONOON ONO NO NO NONOONONOO NONONONON</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingSix">
+						<h5 class="mb-0">
+							<a data-target="#collapseSix" href="javascript:void(0);">
+								<i class="far fa-images"></i>#clubealemão
+							</a>
+						</h5>
+					</div>
+					<div id="collapseSix">
+						<div class="card-body">
+							<h3 class="title_photoSlider">Fotos pendentes: 12</h3>
+							<div id="slide-counter"></div>
+							<div class="photoApproval">
+								<div>
+									<img src="https://placeholdit.co//i/307x276?&bg=FFFFFF&fc=000000&text=Img">
+									<p class="user">@nomeusuário</p>
+									<p class="text">nonononon ono nn o n no nn ono  nonononn non onono nnono n nononononononnonno n no nn ono  nonononn non onono nnono n nononononono n no nn ono  nonononn non onono nnono n nononononono</p>
+								</div>
+								<div>
+									<img src="https://placeholdit.co//i/307x276?&bg=FFFFFF&fc=ED1C24&text=Img">
+									<p class="user">@nomeusuário</p>
+									<p class="text">nonononon ono nn o n no nn ono  nonononn non onono nnono n nononononononnonno n no nn ono  nonononn non onono nnono n nononononono n no nn ono  nonononn non onono nnono n nononononono</p>
+								</div>
+								<div>
+									<img src="https://placeholdit.co//i/307x276?&bg=FFFFFF&fc=FAB701&text=Img">
+									<p class="user">@nomeusuário</p>
+									<p class="text">nonononon ono nn o n no nn ono  nonononn non onono nnono n nononononononnonno n no nn ono  nonononn non onono nnono n nononononono n no nn ono  nonononn non onono nnono n nononononono</p>
+								</div>
+							</div>
+							<div class="approvalBtns">
+								<div class="approve"><a href="#"><i class="far fa-check-circle"></i> Aprovar</a></div>
+								<div class="block"><a href="#"><i class="far fa-times-circle"></i> Bloquear</a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingThree">
+						<h5 class="mb-0">
+							<a data-target="#collapseThree" href="javascript:void(0);">
+								<i class="fas fa-exclamation-triangle"></i>Ocorrências
+							</a>
+						</h5>
+					</div>
+					<div id="collapseThree">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="new_inserts">
+									<h3>Últimas inserções</h3>
+									<ul class="list_guests">
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingSix">
+						<h5 class="mb-0">
+							<a data-target="#collapseSix" href="javascript:void(0);">
+								<i class="fas fa-pencil-ruler"></i>Projetos
+							</a>
+						</h5>
+					</div>
+					<div id="collapseSix">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="new_inserts">
+									<h3>Últimas inserções</h3>
+									<ul class="list_news">
+										<li>
+											<a href="#">
+												<p class="date">22/06/2018</p>
+												<p class="title">NONOONONO nONONONON NO NONONONO NONO</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="date">22/06/2018</p>
+												<p class="title">NONON NON NO ONONONON NONONNON NONOON ON OON NONOON ONO NO NO NONOONONOO NONONONON</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header" id="headingFour">
+						<h5 class="mb-0">
+						<a data-target="#collapseFour" href="javascript:void(0);">
+								<i class="far fa-lightbulb"></i>Sugestões
+							</a>
+						</h5>
+					</div>
+
+					<div id="collapseFour">
+						<div class="card-body">
+							<div class="controls">
+								<div class="insert">
+									<a href="#inserir"><i class="far fa-plus-square"></i> Inserir</a>
+								</div>
+								<div class="edit">
+									<a href="#editar"><i class="far fa-edit"></i> editar</a>
+								</div>
+							</div>
+							<div class="events">
+								<div class="new_inserts">
+									<h3>Últimas inserções</h3>
+									<ul class="list_guests">
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p class="member">fulano de tal - sócio nº xxx</p>
+												<p class="text">onononon ono nn o n no nn ono nononn non onono nnono nononononononnonno onnonnononononon</p>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
